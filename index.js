f6e0dc863f8031e3b03b44b3b8f72f4debab09e6
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2021-11-10 12:26:59 +0100

    Connected to db and promisified the sql query

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require("express");
+const connection = require("./db");
 
 const app = express();
-
 app.use(express.json());
+
+connection.connect((err) => {
+  if (err) {
+    console.error("error connecting to db");
+  } else {
+    console.log("connected to db");
+  }
+});
 
 const things = [
   { id: 1, name: "Socks" },
@@ -36,6 +44,19 @@ app.post("/things", (req, res) => {
 app.get("/", (req, res) => {
   console.log("get was called");
   res.send("Hello from express with nodemon");
+});
+
+app.get("/products", (req, res) => {
+  connection
+    .promise()
+    .query("SELECT * FROM products")
+    .then(([results]) => {
+      res.json(results);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving products from db.");
+    });
 });
 
 app.listen(8000, () => {
